ce4f86c1567c5e3a1c0840675d5bbb79097a4933
sharifuzzaman24 <private.sharifuzzaman@gmail.com>
2024-12-22 15:43:58 +0600

    desinged the featured section and the service card using fake data later replace it to live data from the database

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const ServiceCard = () => {
+const ServiceCard = ({ service }) => {
     return (
-        <div>
-            
+        <div className="bg-white shadow-lg rounded-lg overflow-hidden w-80">
+            <img
+                src={service.image}
+                alt={service.title}
+                className="h-40 w-full object-cover rounded-t-lg"
+            />
+            <div className="p-4">
+                <h2 className="text-xl text-gray-800 font-semibold mb-2">{service.title}</h2>
+                <p className="text-sm text-gray-600 mb-4">{service.description}</p>
+                <p className="text-xl font-bold text-green-500 mb-4">{service.price}</p>
+                <button className="w-full bg-blue-600 text-white py-2 rounded-lg">
+                    See Details
+                </button>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Hero from './Hero';
+import FeaturedSection from './FeaturedSection';
 
 const Home = () => {
     return (
         <>
             <section>
                 <Hero></Hero>
+            </section>
+            <section className='w-4/5 mx-auto'>
+                <FeaturedSection></FeaturedSection>
             </section>
 
         </>
