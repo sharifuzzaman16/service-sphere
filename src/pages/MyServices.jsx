22d247ee501a679425e5af34f39b2bdde46d14ee
sharifuzzaman24 <private.sharifuzzaman@gmail.com>
2024-12-23 13:43:24 +0600

    designed the 404 page

--- src/pages/MyServices.jsx
+++ src/pages/MyServices.jsx
@@ -24,12 +24,22 @@ const MyServices = () => {
 
     return (
         <div className="max-w-7xl mx-auto my-16">
-            <h1 className="text-2xl font-bold mb-6">My Services</h1>
-            <input
-                type="text"
-                placeholder="Search services..."
-                className="mb-6 w-full p-2 border rounded-md"
-            />
+            <div className='flex items-center justify-center flex-col mb-6'>
+            <h1 className="text-2xl text-white font-bold mb-6">My Services</h1>
+            <label className="input input-bordered w-full max-w-xs flex items-center gap-2">
+  <input type="text" className="grow" placeholder="Search" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+      clipRule="evenodd" />
+  </svg>
+</label>
+            </div>
             <table className="w-full text-left border-collapse">
                 <thead>
                     <tr className="bg-[#2F3E46]">
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            404 Not Found
+        <div className="flex items-center justify-center min-h-screen bg-gray-900">
+            <div className="text-center">
+                <h1 className="text-9xl font-bold text-blue-600">404</h1>
+                <p className="text-2xl font-semibold mt-4 text-gray-100">
+                    Oops! Page not found.
+                </p>
+                <p className="mt-2 text-gray-400">
+                    The page you're looking for doesn't exist or has been moved.
+                </p>
+                <div className="mt-6">
+                    <a
+                        href="/"
+                        className="bg-blue-600 text-white px-6 py-3 rounded-md text-lg font-medium shadow-md hover:bg-blue-700"
+                    >
+                        Go Back Home
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
