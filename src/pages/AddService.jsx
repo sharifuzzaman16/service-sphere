1ce40beaeb26f5b263e0c6846f37e1c3ccee7ce3
sharifuzzaman24 <private.sharifuzzaman@gmail.com>
2025-01-11 13:49:53 +0600

    redesigned the add service page

--- src/pages/AddService.jsx
+++ src/pages/AddService.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 import { Helmet } from 'react-helmet-async';
 import useAxiosSecure from '../hooks/useAxiosSecure';
+import { section } from 'framer-motion/client';
 
 const AddService = () => {
 
@@ -23,7 +24,7 @@ const AddService = () => {
         const addedDate = new Date().toISOString();
         const userEmail = user.email;
 
-     
+
         if (!serviceImage || !serviceTitle || !companyName || !category || !price) {
             Swal.fire({
                 position: "center",
@@ -82,72 +83,71 @@ const AddService = () => {
     }
 
     return (
-        <div className="max-w-4xl my-16 mx-auto p-8 bg-[#2F3E46] shadow-md rounded-md">
-            <Helmet>
-                <title>Add Service - ServiceSphere</title>
-            </Helmet>
-            <h1 className="text-2xl font-bold text-white text-center mb-6">Add a New Service</h1>
-            <form onSubmit={handleAddService} className="space-y-4">
-                <div>
-                    <label className="block text-sm font-medium text-gray-300 mb-2">Service Image</label>
-                    <input name='serviceImage' type="url" placeholder="Service image URL" className="input input-bordered w-full" />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-300 mb-2">Service Title</label>
-                    <input name='serviceTitle' type="text" placeholder="Service title" className="input input-bordered w-full" />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-300 mb-2">Company Name</label>
-                    <input name='companyName' type="text" placeholder="Company name" className="input input-bordered w-full" />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-300 mb-2">Website</label>
-                    <input name='website' type="url" placeholder="Website URL" className="input input-bordered w-full" />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-300 mb-2">Description</label>
-                    <textarea name='description' className="w-full textarea textarea-bordered" placeholder="Description"></textarea>
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-300 mb-2">Category</label>
-                    <select
-                        name="category"
-                        className="select select-bordered w-full"
-                    >
-                        <option disabled value="">Select a category</option>
-                        <option value="technology">Technology and IT</option>
-                        <option value="healthcare">Healthcare</option>
-                        <option value="education">Education</option>
-                        <option value="finance">Finance and Accounting</option>
-                        <option value="marketing">Marketing and Sales</option>
-                        <option value="engineering">Engineering</option>
-                        <option value="design">Creative and Design</option>
-                        <option value="construction">Construction and Real Estate</option>
-                        <option value="hospitality">Hospitality and Tourism</option>
-                        <option value="administration">Administrative and Support Services</option>
-                        <option value="legal">Legal</option>
-                        <option value="media">Media and Communications</option>
-                        <option value="science">Science and Research</option>
-                        <option value="transport">Transport and Logistics</option>
-                        <option value="retail">Retail and Customer Service</option>
-                        <option value="trades">Skilled Trades</option>
-                        <option value="remote">Remote and Freelance Jobs</option>
-                        <option value="green">Green and Sustainable Jobs</option>
-                        <option value="ecommerce">E-commerce</option>
-                    </select>
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-300 mb-2">Price</label>
-                    <input name='price' type="number" placeholder="Price" className="input input-bordered w-full" />
-                </div>
-                <button
-                    type="submit"
-                    className="bg-blue-600 w-full text-white px-4 py-3 rounded-md hover:bg-blue-700"
-                >
-                    Add Service
-                </button>
-            </form>
-        </div>
+        <section className='py-32'>
+            <div className="max-w-4xl mx-auto p-8 bg-white shadow-md rounded-md">
+                <Helmet>
+                    <title>Add Service - ServiceSphere</title>
+                </Helmet>
+                <h1 className="text-2xl font-bold text-[#2F3640] text-center mb-6">Add a New Service</h1>
+                <form onSubmit={handleAddService} className="space-y-4">
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">Service Image</label>
+                        <input name='serviceImage' type="url" placeholder="Service image URL" className="input input-bordered w-full" />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">Service Title</label>
+                        <input name='serviceTitle' type="text" placeholder="Service title" className="input input-bordered w-full" />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">Company Name</label>
+                        <input name='companyName' type="text" placeholder="Company name" className="input input-bordered w-full" />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">Website</label>
+                        <input name='website' type="url" placeholder="Website URL" className="input input-bordered w-full" />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">Description</label>
+                        <textarea name='description' className="w-full textarea textarea-bordered" placeholder="Description"></textarea>
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">Category</label>
+                        <select
+                            name="category"
+                            className="select select-bordered w-full"
+                        >
+                            <option disabled value="">Select a category</option>
+                            <option value="technology">Technology and IT</option>
+                            <option value="healthcare">Healthcare</option>
+                            <option value="education">Education</option>
+                            <option value="finance">Finance and Accounting</option>
+                            <option value="marketing">Marketing and Sales</option>
+                            <option value="engineering">Engineering</option>
+                            <option value="design">Creative and Design</option>
+                            <option value="construction">Construction and Real Estate</option>
+                            <option value="hospitality">Hospitality and Tourism</option>
+                            <option value="administration">Administrative and Support Services</option>
+                            <option value="legal">Legal</option>
+                            <option value="media">Media and Communications</option>
+                            <option value="science">Science and Research</option>
+                            <option value="transport">Transport and Logistics</option>
+                            <option value="retail">Retail and Customer Service</option>
+                            <option value="trades">Skilled Trades</option>
+                            <option value="remote">Remote and Freelance Jobs</option>
+                            <option value="green">Green and Sustainable Jobs</option>
+                            <option value="ecommerce">E-commerce</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">Price</label>
+                        <input name='price' type="number" placeholder="Price" className="input input-bordered w-full" />
+                    </div>
+                    <button className="px-6 py-3 rounded-md bg-[#1E8449] text-white font-semibold shadow-lg hover:bg-[#F39C12] transition w-full">
+                        Add Service
+                    </button>
+                </form>
+            </div>
+        </section>
     );
 };
 
